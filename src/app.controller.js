571e52777bed1aff3c1e728d7262b11875e02a5c
Mohamed Shawky <64426063+hamo55530@users.noreply.github.com>
2026-02-01 02:00:17 +0200

    editing app.controller

--- src/app.controller.js
+++ src/app.controller.js
@@ -4,26 +4,21 @@ import connectDB from "./database/connection.js";
 import userRoutes from "./modules/user/user.routes.js";
 import adminRoutes from "./modules/admin/admin.routes.js";
 import { ENV } from "../config/env.service.js";
-
 export const bootstrap = async() => {
   const app = express();
-  app.options("*", cors());
   app.use(
     cors({
-      origin: "*",
+      origin: "http://localhost:3000",
       methods: ["GET", "POST", "PUT", "DELETE"],
       allowedHeaders: ["Content-Type", "Authorization", "Accept"],
       optionsSuccessStatus: 200,
     })
   );
-
+  app.options("*", cors());
   app.use(express.json());
-
   await connectDB();
-
   app.use("/api/users", userRoutes);
   app.use("/api/admin", adminRoutes);
-
   app.listen(ENV.PORT, () =>
     console.log(`🚀 Server running on port ${ENV.PORT}`)
   );
